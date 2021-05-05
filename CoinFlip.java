335d282587ddc2ee26fc31e14787e94e7c326aac
Liolf3 <83280511+Liolf3@users.noreply.github.com>
2021-05-05 16:37:16 +0300

    Update CoinFlip.java

--- CoinFlip.java
+++ CoinFlip.java
@@ -5,15 +5,24 @@ import java.util.*;
 
 public class CoinFlip {
     public static void main(String[] args) {
+        Scanner cn = new Scanner(System.in);
+        System.out.println("Heads or tails?");
+        String s = cn.nextLine();
+
         Random rand = new Random();
-        String h = "Heads!";
-        String t = "Tails!";
-        int coin = rand.nextInt(2) + 1;
-        if (coin == 1) {
-        System.out.println(h);
+        int c = rand.nextInt(2) + 1;
+
+        if (c == 1 && s.equalsIgnoreCase("heads")) {
+            System.out.println("Yay! It's heads!");
         }
-        else {
-            System.out.println(t);
+        else if (c == 1 && s.equalsIgnoreCase("tails")) {
+            System.out.println("Nope! It's heads!");
+        }
+        else if (c == 2 && s.equalsIgnoreCase("tails")) {
+            System.out.println("Yeah! It's tails.");
+        }
+        else if (c == 2 && s.equalsIgnoreCase("heads")) {
+            System.out.println("Nope! It's tails.");
         }
     }
 }
